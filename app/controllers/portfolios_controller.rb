b1211852704d6d36cad83e31cfbc54ebbd3cdb90
Chris Hanson <>
2018-07-12 05:47:58 +0930

    add friendly routes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,6 +8,10 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
 
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
@@ -26,12 +30,23 @@ class PortfoliosController < ApplicationController
 
   def update
     @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item = Portfolio.find(params[:id])
+        @portfolio_item.destroy
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'Portfolio item created' }
       else
         format.html { render :new }
       end
+    end
+  end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Item deleted' }
     end
   end
 
