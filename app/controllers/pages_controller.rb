1956408ad72c96959fc1f9a7e6d3329431bbb097
Chris Hanson <>
2018-08-29 05:21:57 +0930

    contect form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,9 +6,28 @@ class PagesController < ApplicationController
     @jobs = Job.all
     @blogs = Blog.limit(3).includes(:topic)
     @portfolio_items = Portfolio.all.includes(:technologies)
+    @contact = Contact.new(params[:contact])
   end
 
   def privacy
+  end
+
+  def create_contact
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+
+    byebug
+
+    respond_to do |format|
+      if @contact.deliver
+        @contact = Contact.new
+        format.html { render 'index'}
+        format.js   { flash.now[:success] = "Thank you for your message. I'll get back to you soon!" }
+      else
+        format.html { render 'index' }
+        format.js   { flash.now[:error] = "Message did not send." }
+      end
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,27 @@
 Rails.application.routes.draw do
   namespace :admin do
-      resources :users
-      resources :blogs
-      resources :jobs
-      resources :portfolios
-      resources :skills
-      resources :technologies
-      resources :topics
-      resources :guest_users
+    resources :users
+    resources :blogs
+    resources :jobs
+    resources :portfolios
+    resources :skills
+    resources :technologies
+    resources :topics
+    resources :guest_users
 
-      root to: "users#index"
-    end
+    root to: "users#index"
+  end
+
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+
   root to: 'pages#home'
-
-  resources :portfolios, except: [:show]
+  
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-
   get 'privacy', to: 'pages#privacy'
   get 'topics/:id', to: 'topics#show', as: 'topic_show'
+  post 'contact', to: 'pages#create_contact', as: 'home_contact'
+  
+  resources :portfolios, except: [:show]
 
   resources :blogs do
     member do
